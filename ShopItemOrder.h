d51ac9eef8727548cc19dcf3ad238103458ba643
Blake Vance <nsdnmplz@gmail.com>
2017-10-02 14:42:14 -0400

    Merge origin/master
    Conflicts:
    	README.md

---
+++ ShopItemOrder.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   ShopItemOrder.h
+ * Author: bvance
+ *
+ * Created on September 27, 2017, 1:05 PM
+ */
+
+#ifndef SHOPITEMORDER_H
+#define SHOPITEMORDER_H
+
+class ShopItemOrder {
+public:
+    ShopItemOrder();
+    ShopItemOrder(const ShopItemOrder& orig);
+    virtual ~ShopItemOrder();
+private:
+
+};
+
+#endif /* SHOPITEMORDER_H */
+
